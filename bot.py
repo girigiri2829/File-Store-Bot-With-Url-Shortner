9926fa5c873b1f9d69fc565087087f1746710e33
Sridharan <128954293+girigiri2829@users.noreply.github.com>
2025-05-16 18:12:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,29 +91,11 @@ async def start(bot: Client, cmd: Message):
         if START_PIC:  # Check if START_PIC has a value
             await message.reply_photo(
                 photo=START_PIC,
-                caption=START_MSG.format(
-                    first=message.from_user.first_name,
-                    last=message.from_user.last_name,
-                    username=None if not message.from_user.username else '@' + message.from_user.username,
-                    mention=message.from_user.mention,
-                    id=message.from_user.id
-                ),
+                caption=HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
                 reply_markup=reply_markup,
-                quote=True
+               
             )
-        else:  # If START_PIC is empty, send only the text
-            await message.reply_text(
-                text=START_MSG.format(
-                    first=message.from_user.first_name,
-                    last=message.from_user.last_name,
-                    username=None if not message.from_user.username else '@' + message.from_user.username,
-                    mention=message.from_user.mention,
-                    id=message.from_user.id
-                ),
-                reply_markup=reply_markup,
-                disable_web_page_preview=True,
-                quote=True
-            )
+        return
     else:
         try:
             try:
