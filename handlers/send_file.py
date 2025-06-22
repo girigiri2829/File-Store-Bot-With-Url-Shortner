974020d82dce61f29ae53de7cede7a2e2a9266b2
Sridharan <128954293+girigiri2829@users.noreply.github.com>
2025-06-22 11:09:44 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,7 +11,7 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
+            f"**Sorry! This File will Be deleted In 30 Minutes To Avoid Copyright Issues Please Forward This File Immediately And Save Them.**",
             disable_web_page_preview=True,
             quote=True
         )
